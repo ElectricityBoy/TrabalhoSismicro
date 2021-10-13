d3c44856b3ecd40339608ae6d3b186c9a73dc005
Elder Pereira <elder-guedes@hotmail.com>
2021-10-13 19:23:38 -0300

    Atualização do data logger

--- DHT22_LCD16X2/main.c
+++ DHT22_LCD16X2/main.c
@@ -21,14 +21,6 @@
 int sensor = 0;
 char adc[5];
 
-void EEPROM_escrita(unsigned int endereco,  float dado)
-{ while(EECR & (1<<EEPE));//espera completar um escrita prévia
-	EEAR = endereco; //carrega o endereço para a escrita
-	EEDR = dado; //carrega o dado a ser escrito
-	EECR |= (1<<EEMPE); //escreve um lógico em EEMPE
-	EECR |= (1<<EEPE); //inicia a escrita ativando EEPE
-}
-
 
 
 
@@ -38,12 +30,6 @@ int main(void)
 	float temperatura;
 	float humedad;
 	
-	unsigned char EEPROM_leitura(unsigned int endereco)
-	{ while(EECR & (1<<EEPE));//espera completar um escrita prévia
-		EEAR = endereco; //escreve o endereço de leitura
-		EECR |= (1<<EERE); //inicia a leitura ativando EERE
-		return EEDR; //retorna o valor lido do registrador
-	}
 	
 	adc_referencia(1); // Para o valor de referencia 1, REF.INTERNA 5V
 	adc_iniciar();
@@ -54,32 +40,37 @@ int main(void)
 	UART_init();
 	DHT22_init();
 	
-	
     while (1) 
     {	
 		
 		
 		contador++;
-		if(contador>= 100){			//Para ler o DHT22 a cada 200x10ms = 2000ms e nao utilizar retardos bloqueantes de 2s
+		if(contador>= 100.00){			//Para ler o DHT22 a cada 200x10ms = 2000ms e nao utilizar retardos bloqueantes de 2s
 			contador=0;
 			
 			adc_canal(0);
 			sensor = adc_start();
 			itoa(sensor,adc,10);
 			uint8_t status = DHT22_read(&temperatura, &humedad);
+			
 			if (status)
 			{
 			
 				if (eeprom_is_ready() == 1)
-				{
+				{	
+
+						
 					
+					eeprom_write_word(contador,adc);
+					printf("\r");
+					printf("Luminosidade : %s.2\n\r",eeprom_read_word(contador));
 					eeprom_write_float(contador,temperatura);
+					printf("Temperatura (Cº): %2.2f\n\r", eeprom_read_float(contador) );
+					//printf("EEPROM : %2.2f\n\r", eeprom_read_float(contador));
+					eeprom_write_float(contador,humedad);
+					printf("Umidade : %.2lf\n\r", eeprom_read_float(contador));
 					printf("\r");
-					printf("Luminosidade : %s.2\n\r", adc );
-					printf("Temperatura (Cº): %2.2f\n\r", temperatura );
-					printf("EEPROM : %2.2f\n\r", eeprom_read_float(contador));
-					printf("Umidade : %.2lf\n\r", humedad);
-					printf("\r");
+					//Os valores registrados no terminal virtual, são os valores lidos através do endereço de memoria correspondente da memória EEPROM.
 					
 					LCD_clear();
 					LCD_printf(" Temp. ");
@@ -98,14 +89,10 @@ int main(void)
 			}
 			else
 			{
-				//LCD_clear();
-				//LCD_printf("Error");
-				//printf("ERROR\n\r");
 				LCD_clear();
-				LCD_printf(" Temp. ");
-				dtostrf(EEPROM_leitura(0), 2, 2, printbuff);
-				LCD_printf(printbuff);
-				LCD_printf(" C");
+				LCD_printf("Error");
+				printf("ERROR\n\r");
+				//Caso não
 				
 			}
 			
